11172ce00a667ef51f1e007dff3e312426084315
AgathaGoncalves <71910283+AgathaGoncalves@users.noreply.github.com>
2024-04-06 00:09:47 -0300

    Feat: Implementing http types

---
+++ src/http_types/http_request.py
@@ -0,0 +1,7 @@
+# pylint: disable-all
+from typing import Dict
+
+class HttpRequest:
+    def __init__(self, body: Dict = None, param: Dict = None)-> None:
+        self.body = body
+        self.param = param
--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,8 +1,10 @@
 # pylint: disable-all
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
 
 event_route_bp = Blueprint('event_route', __name__)
 
 @event_route_bp.route('/events', methods=['POST'])
 def get_events():
+    http_request = HttpRequest(body=request.json)
     return  jsonify({"helooooo": "woooord"}),200
